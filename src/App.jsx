c86b34f9b591531f021b5babe64335c42163bc0c
diegogaldino-dev <68260569+diegogaldino-dev@users.noreply.github.com>
2022-09-21 21:11:12 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// react, componentes, estáticos
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
